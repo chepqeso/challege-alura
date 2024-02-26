90ca5e6661513c38da3d71225e201171550cf233
Ronald <elbetova95@gmail.com>
2024-02-25 23:09:12 -0600

    funciones-completas

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const texto = document.getElementById('txt-encriptar');
-const mesaje = document.getElementById('mesaje-salida');
+const mensaje = document.getElementById('mensaje-salida');
 let matrizCode = [
     ['e','enter'],
     ['i','imes'],
@@ -10,15 +10,14 @@ let matrizCode = [
 //funcion para cifrar el mensaje
 function btnEncriptar(){
     const encriptarOn = encriptar(texto.value);
-    mesaje.value = encriptarOn;
-} 
-
+    mensaje.value = encriptarOn;
+    
     document.getElementById('homero-1').style.display = 'none';
     document.getElementById('mensaje-1').style.display = 'none';
     document.getElementById('mensaje-2').style.display = 'none';
     document.getElementById('btncopiar').style.display = 'inherit';
-    document.getElementById('mesaje-salida').style.display = 'inherit';
-    //document.getElementById('mesaje-salida').innerHTML = ;
+    document.getElementById('mensaje-salida').style.display = 'inherit';
+}
 function encriptar(fraseEncriptada){
     for (let i = 0; i < matrizCode.length; i++){
         if(fraseEncriptada.includes(matrizCode[i][0])){
@@ -29,4 +28,30 @@ function encriptar(fraseEncriptada){
         }
     }
     return fraseEncriptada;
+}
+function btnDesencriptar(){
+    const desencriptarOff = desencriptar(texto.value)
+    mensaje.value = desencriptarOff;
+
+    document.getElementById('homero-1').style.display = 'none';
+    document.getElementById('mensaje-1').style.display = 'none';
+    document.getElementById('mensaje-2').style.display = 'none';
+    document.getElementById('btncopiar').style.display = 'inherit';
+    document.getElementById('mensaje-salida').style.display = 'inherit';
+}
+
+function desencriptar(cadenaCodificada){
+    for (let i = 0; i <  matrizCode.length; i++){
+        if(cadenaCodificada.includes(matrizCode[i][1])){
+            cadenaCodificada = cadenaCodificada.replaceAll(
+                matrizCode[i][1],
+                matrizCode[i][0]
+            );
+        }
+    }
+    return cadenaCodificada;
+}
+function copiarTexto(){
+    navigator.clipboard.writeText(mensaje.value);
+    alert("Se ha copiado el texto")
 }
